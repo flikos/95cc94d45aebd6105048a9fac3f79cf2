67fa241307ed444f34342f7ca0b9b1ccc0985f30
flikos <m9273317881@gmail.com>
2021-06-18 21:09:57 +0500

    base stage

--- app/alytics/models.py
+++ app/alytics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class GraphFunc(models.Model):
+    title = models.CharField(max_length=100)
+    graphic = models.ImageField()
+    func_exception = models.TextField(null=True, blank=True, editable=False)
+    interval = models.IntegerField()
+    dt = models.IntegerField()
+    create_datetime = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return str(self.title)
--- app/testwork/settings.py
+++ app/testwork/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'alytics.apps.AlyticsConfig',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
